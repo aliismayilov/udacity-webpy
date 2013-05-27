52526901fba283a7ad8c4d46c706b68d8d49e9c8
Ali Ismayilov <ali@ismailov.info>
2013-05-27 15:28:54 +0500

    simplify

--- helloworld.py
+++ helloworld.py
@@ -16,16 +16,6 @@ class MainPage(webapp2.RequestHandler):
         self.response.out.write(template.render())
 
 
-class TestHandler(webapp2.RequestHandler):
-    def post(self):
-        q = self.request.get("q")
-        self.response.out.write(q)
-
-        # self.response.headers['Content-Type'] = 'text/plain'
-        # self.response.out.write(self.request)
-
-
 application = webapp2.WSGIApplication([
     ('/', MainPage),
-    ('/testform', TestHandler),
 ], debug=True)
