ed0e12efb15fe7631cf9a4f529aac29cf128e347
Ali Ismayilov <ali@ismailov.info>
2013-05-27 23:11:47 +0500

    use jinja escaping and built-in rot13 encoding

--- helloworld.py
+++ helloworld.py
@@ -2,12 +2,12 @@ import os
 
 import webapp2
 import jinja2
-import cgi
 
 
 JINJA_ENVIRONMENT = jinja2.Environment(
     loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
-    extensions=['jinja2.ext.autoescape']
+    extensions=['jinja2.ext.autoescape'],
+    autoescape=True
 )
 
 
@@ -21,36 +21,20 @@ class Rot13(webapp2.RequestHandler):
     def get(self):
         template = JINJA_ENVIRONMENT.get_template('rot13.html')
         context = {
-            'text_value': ""
+            'text': ""
         }
         self.response.out.write(template.render(context))
 
     def post(self):
         template = JINJA_ENVIRONMENT.get_template('rot13.html')
+        text = self.request.get('text')
+        if text:
+            text = text.encode('rot13')
+
         context = {
-            'text_value': cgi.escape(rot13(self.request.get('text')))
+            'text': text
         }
         self.response.out.write(template.render(context))
-
-
-def rot13(string):
-    result = ''
-    for c in string:
-        if not c.isalpha():
-            result += c
-            continue
-
-        if ord(c) >= ord('A') and ord(c) <= ord('Z'):
-            c13 = ord(c) + 13
-            if c13 > ord('Z'):
-                c13 -= 26
-            result += chr(c13)
-        elif ord(c) >= ord('a') and ord(c) <= ord('z'):
-            c13 = ord(c) + 13
-            if c13 > ord('z'):
-                c13 -= 26
-            result += chr(c13)
-    return result
 
 
 application = webapp2.WSGIApplication([
