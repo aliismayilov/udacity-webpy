47c71f764b462b6196a573e8a75f455d7c84bcd3
Ali Ismayilov <ali@ismailov.info>
2013-05-27 23:03:13 +0500

    complete rot13 homework

--- helloworld.py
+++ helloworld.py
@@ -2,6 +2,7 @@ import os
 
 import webapp2
 import jinja2
+import cgi
 
 
 JINJA_ENVIRONMENT = jinja2.Environment(
@@ -16,6 +17,42 @@ class MainPage(webapp2.RequestHandler):
         self.response.out.write(template.render())
 
 
+class Rot13(webapp2.RequestHandler):
+    def get(self):
+        template = JINJA_ENVIRONMENT.get_template('rot13.html')
+        context = {
+            'text_value': ""
+        }
+        self.response.out.write(template.render(context))
+
+    def post(self):
+        template = JINJA_ENVIRONMENT.get_template('rot13.html')
+        context = {
+            'text_value': cgi.escape(rot13(self.request.get('text')))
+        }
+        self.response.out.write(template.render(context))
+
+
+def rot13(string):
+    result = ''
+    for c in string:
+        if not c.isalpha():
+            result += c
+            continue
+
+        if ord(c) >= ord('A') and ord(c) <= ord('Z'):
+            c13 = ord(c) + 13
+            if c13 > ord('Z'):
+                c13 -= 26
+            result += chr(c13)
+        elif ord(c) >= ord('a') and ord(c) <= ord('z'):
+            c13 = ord(c) + 13
+            if c13 > ord('z'):
+                c13 -= 26
+            result += chr(c13)
+    return result
+
+
 application = webapp2.WSGIApplication([
-    ('/', MainPage),
+    ('/', Rot13),
 ], debug=True)
