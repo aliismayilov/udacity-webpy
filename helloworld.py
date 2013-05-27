e469e01d88eb5599f428ee7dc473adf451945a93
Ali Ismayilov <ali@ismailov.info>
2013-05-27 14:33:54 +0500

    reindent 4 spaces

--- helloworld.py
+++ helloworld.py
@@ -5,27 +5,27 @@ import jinja2
 
 
 JINJA_ENVIRONMENT = jinja2.Environment(
-  loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
-  extensions=['jinja2.ext.autoescape']
+    loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
+    extensions=['jinja2.ext.autoescape']
 )
 
 
 class MainPage(webapp2.RequestHandler):
-  def get(self):
-    template = JINJA_ENVIRONMENT.get_template('play.html')
-    self.response.out.write(template.render())
+    def get(self):
+        template = JINJA_ENVIRONMENT.get_template('play.html')
+        self.response.out.write(template.render())
 
 
 class TestHandler(webapp2.RequestHandler):
-  def get(self):
-    q = self.request.get("q")
-    self.response.out.write(q)
+    def post(self):
+        q = self.request.get("q")
+        self.response.out.write(q)
 
-    # self.response.headers['Content-Type'] = 'text/plain'
-    # self.response.out.write(self.request)
+        # self.response.headers['Content-Type'] = 'text/plain'
+        # self.response.out.write(self.request)
 
 
 application = webapp2.WSGIApplication([
-  ('/', MainPage),
-  ('/testform', TestHandler),
+    ('/', MainPage),
+    ('/testform', TestHandler),
 ], debug=True)
